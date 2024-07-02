c76dc0e75246ffd358b84a4a0c53d442268c22d6
Commmodore64 <cesarml2002@hotmail.com>
2024-07-02 14:19:31 -0600

    Funcionalidad basica Auth0

--- ujed/src/App.js
+++ ujed/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { useAuth0 } from '@auth0/auth0-react';
+import Profile from './components/Profile';
+import LogoutButton from './components/LogoutButton';
 
 function App() {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
   return (
     <div className="App">
       <h1>React App</h1>
       <button onClick={() => loginWithRedirect()}>login</button>
+      <Profile />
+      {isAuthenticated && <LogoutButton />}
     </div>
   );
 }
