46c15b88b0b786bacefcd3aeb14e1eda54d90f8d
Commmodore64 <cesarml2002@hotmail.com>
2024-07-02 13:30:15 -0600

    first commit, auth0 n tailwindcss

---
+++ ujed/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
+
+function App() {
+  const { loginWithRedirect } = useAuth0();
+  return (
+    <div className="App">
+      <h1>React App</h1>
+      <button onClick={() => loginWithRedirect()}>login</button>
+    </div>
+  );
+}
+
+export default App;
