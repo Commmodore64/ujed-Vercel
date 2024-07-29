6549838492fe9cea5749f775be81d562f9e2c9ce
Commmodore64 <cesarml2002@hotmail.com>
2024-07-29 10:54:11 -0600

    pwa icon

--- ujed/src/components/LogoutButton.js
+++ ujed/src/components/LogoutButton.js
@@ -6,7 +6,7 @@ const LogoutButton = ({className}) => {
   return (
     <button
       className={`m-5 bg-gray-900 hover:bg-[#B11830] transition duration-300 ease-in-out text-white py-2 px-4 rounded-md ${className}`}
-      onClick={() => logout({ returnTo: "/" })}
+      onClick={() => logout({ returnTo: window.location.origin })}
     >
       Cerrar Sesión
     </button>
