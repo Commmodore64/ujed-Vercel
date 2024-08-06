f1433e57a9085f42d154cfa408ddce0c03561b78
Commmodore64 <cesarml2002@hotmail.com>
2024-08-06 14:00:26 -0600

    5/Ago Finished

--- ujed/src/components/history/Index.js
+++ ujed/src/components/history/Index.js
@@ -84,7 +84,7 @@ const Index = () => {
           <h1 className="text-2xl font-bold">Inscripción</h1>
           <div className="w-full">
             <div className="flex flex-col md:flex-row items-start md:items-center justify-between mb-4">
-              <div className="flex items-center gap-2 w-full md:w-auto">
+              <div className="flex items-center gap-2 w-full md:w-auto mt-4">
                 <input
                   className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 flex-1 md:flex-none"
                   placeholder="Search by name..."
